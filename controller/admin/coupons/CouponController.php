7a43707d9b4d68b4ff2092f380c85b2f73657c6e
belal-shakra <belal.a.shakra@gmail.com>
2024-10-27 12:24:49 +0300

    CRUD on Coupon

---
+++ controller/admin/coupons/CouponController.php
@@ -0,0 +1,32 @@
+<?php
+
+require "./model/Coupon.php";
+
+class CouponController {
+
+  private $coupon;
+
+  function __construct(){
+    $this->coupon = new Coupon();
+  }
+
+
+  public function index(){
+    return $this->coupon->where("SELECT * FROM coupons ORDER BY status");
+  }
+
+  public function create($data){
+    $this->coupon->create($data);
+  }
+
+  public function find($pk){
+    return $this->coupon->find($pk);
+  }
+
+  public function update($data, $pk){
+    $this->coupon->update($data, $pk);
+  }
+
+
+
+}

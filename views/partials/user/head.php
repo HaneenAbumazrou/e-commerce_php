e71bc215a6703e96e4afdcd9db440fe70a92d050
belal-shakra <belal.a.shakra@gmail.com>
2024-10-28 16:43:35 +0300

    fix assets bug

--- views/partials/user/head.php
+++ views/partials/user/head.php
@@ -7,24 +7,24 @@
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="public/user/assets/css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="public/user/assets/css/animate.css">
+    <link rel="stylesheet" href="/public/user/assets/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="/public/user/assets/css/animate.css">
     
-    <link rel="stylesheet" href="public/user/assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="public/user/assets/css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="public/user/assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="/public/user/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/public/user/assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="/public/user/assets/css/magnific-popup.css">
 
-    <link rel="stylesheet" href="public/user/assets/css/aos.css">
+    <link rel="stylesheet" href="/public/user/assets/css/aos.css">
 
-    <link rel="stylesheet" href="public/user/assets/css/ionicons.min.css">
+    <link rel="stylesheet" href="/public/user/assets/css/ionicons.min.css">
 
-    <link rel="stylesheet" href="public/user/assets/css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="public/user/assets/css/jquery.timepicker.css">
+    <link rel="stylesheet" href="/public/user/assets/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="/public/user/assets/css/jquery.timepicker.css">
 
     
-    <link rel="stylesheet" href="public/user/assets/css/flaticon.css">
-    <link rel="stylesheet" href="public/user/assets/css/icomoon.css">
-    <link rel="stylesheet" href="public/user/assets/css/style.css">
+    <link rel="stylesheet" href="/public/user/assets/css/flaticon.css">
+    <link rel="stylesheet" href="/public/user/assets/css/icomoon.css">
+    <link rel="stylesheet" href="/public/user/assets/css/style.css">
 
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"> -->
 </head>
